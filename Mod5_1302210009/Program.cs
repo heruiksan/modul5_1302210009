f331fee9382077b155da4025420b2167b4998ada
re11n <ryansiahaan@student.telkomuniversity.ac.id>
2023-03-24 09:19:11 +0700

    implementasi-generic-class

--- Mod5_1302210009/Program.cs
+++ Mod5_1302210009/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Mod5_1302210009;
+
+
+SimpleDataBase<int> data = new SimpleDataBase<int>();
+data.AddNewData(13);
+data.AddNewData(02);
+data.AddNewData(21);
+data.printAllData();
---
+++ Mod5_1302210009/SimpleDataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod5_1302210009
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData { get; set; }
+        private List<DateTime> inputDates { get; set; }
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData()
+        {
+            for (int i = 0;i< storedData.Count; i++)
+            {
+                Console.WriteLine("Data"+ (i+1) + "berisi" + storedData.ElementAt(i) + "Yang di simpan pada waktu UTC : " +inputDates.ElementAt(i)  );
+            }
+        }
+    }
+}
