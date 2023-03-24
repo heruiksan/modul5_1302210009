f62a88177c338786557c60b9f35223a13f78e537
re11n <ryansiahaan@student.telkomuniversity.ac.id>
2023-03-24 09:25:24 +0700

    final

--- Mod5_1302210009/Program.cs
+++ Mod5_1302210009/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Mod5_1302210009;
+long a = 13;
+long b = 02;
+long c = 21;
 
+Penjumlahan<long> hasil = new Penjumlahan<long>();
+hasil.JumlahTigaAngka(a, b, c);
 
 SimpleDataBase<int> data = new SimpleDataBase<int>();
 data.AddNewData(13);
